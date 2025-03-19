ec48800c1002756ec0d29846da5a0d61cde7e0f2
KevinFiocre <kevinfiocre@gmail.com>
2025-03-19 15:13:14 +0100

    Ecran blanc lors du test, probleme dans la console, probleme dans index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";  // Assure-toi que ce fichier existe bien
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
 );
